58bb8a7910b6d8eabeda1c49af9528d550ef2006
RickyMartin12 <ricardopeleira16@gmail.com>
2024-05-27 13:01:59 +0100

    fola55

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(function (req, res, next) {
     //Enabling CORS
     res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
       next();
     });
 
