5a03368ff9204140881f54fad9ebedf99d7c5dce
RickyMartin12 <ricardopeleira16@gmail.com>
2024-05-27 12:58:20 +0100

    fola4

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ function getSongDetails(type, dir){
 app.use(function (req, res, next) {
     //Enabling CORS
     res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
       next();
     });
